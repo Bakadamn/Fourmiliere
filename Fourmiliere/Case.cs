337ee4bb1575509b58c193f62615a6b334c057c9
Bastoboy <bakadamned@gmail.com>
2021-04-19 10:54:23 +0200

    ajout classe

---
+++ Fourmiliere/Case.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourmiliere
+{
+    class Case
+    {
+        public char contenu;
+        public int nombre_sucre;
+        public int pheromone_nid;
+        public int pheromone_sucre;
+        
+    }
+}
