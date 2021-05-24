bece00950de6389dbbec469d9e1fa9c3b1bd4c93
Bastoboy <bakadamned@gmail.com>
2021-05-24 10:09:36 +0200

    AJout ia Foumi

--- Fourmiliere/Fourmis.cs
+++ Fourmiliere/Fourmis.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 
@@ -22,7 +20,6 @@ namespace Fourmiliere
         private bool chercheNid;
         public Case caseFourmi;
 
-        private int distanceNid;
         private int pheroSucreVal;
 
         public Fourmis(Case ca)
@@ -43,15 +40,15 @@ namespace Fourmiliere
             List<Case> listeCaseNid = new List<Case>();
 
 
-            
+
 
             int cptCasesValides = 0;
-            foreach(Case ca in CaisseAOut.CasesAlentours(caseFourmi.X, caseFourmi.Y))
+            foreach (Case ca in CaisseAOut.CasesAlentours(caseFourmi.X, caseFourmi.Y))
             {
                 if (ca.pheromone_sucre > 0)
                     listePheroSucre.Add(ca);
                 if (ca.pheromone_nid > 0)
-                    listePheroNid.Add(ca);  
+                    listePheroNid.Add(ca);
                 if (ca.nombre_sucre > 0)
                     listeCaseSucre.Add(ca);
                 if (ca.contenu == 'N')
@@ -62,8 +59,8 @@ namespace Fourmiliere
 
 
 
-         
-            if(chercheSucre && listeCaseSucre.Count()>0) // si un cherche sucre et qu'un sucre est a proximité, on charge la mule
+
+            if (chercheSucre && listeCaseSucre.Count() > 0) // si un cherche sucre et qu'un sucre est a proximité, on charge la mule
             {
                 int nidX = Tableau.posNidStatic[0];
                 int nidY = Tableau.posNidStatic[1];
@@ -74,34 +71,37 @@ namespace Fourmiliere
                 if (nidX > caseFourmi.X)
                     distanceX = nidX - caseFourmi.X;
                 else
-                    distanceX =  caseFourmi.X - nidX;
+                    distanceX = caseFourmi.X - nidX;
 
                 if (nidY > caseFourmi.Y)
                     distanceY = nidY - caseFourmi.Y;
                 else
                     distanceY = caseFourmi.Y - nidY;
 
-                //La valeur du phéromone sucre est la distance entre le nid et le sucre fois deux (le temps de l'allez-retour) + 3 (reste trois tours supplémentaire)
-                
+                //La valeur du phéromone sucre est la distance entre le nid et le sucre fois deux (le temps de l'allez-retour) + 3 (plus trois tours supplémentaire)
+
                 if (distanceX > distanceY)
-                    pheroSucreVal = distanceX*2+3;
+                    pheroSucreVal = distanceX * 2 + 3;
                 else
-                    pheroSucreVal = distanceY*2+3;
-                
+                    pheroSucreVal = distanceY * 2 + 3;
 
 
-                listeCaseSucre[0].nombre_sucre--;
-                if (listeCaseSucre[0].nombre_sucre <= 0)
-                    listeCaseSucre[0].contenu = '0';
-                porteSucre = true;
-                chercheSucre = false;
-                chercheNid = true;
-               
+
+                if (listeCaseSucre[0].nombre_sucre > 0)
+                {
+                    listeCaseSucre[0].nombre_sucre--;
+                    if (listeCaseSucre[0].nombre_sucre <= 0)
+                        listeCaseSucre[0].contenu = '0';
+                    porteSucre = true;
+                    chercheSucre = false;
+                    chercheNid = true;
+
+                }
                 return;
             }
 
-            
-            if(chercheNid && listeCaseNid.Count()>0) // si cherche le nid et qu'il est a proximité, on décharge la mule
+
+            if (chercheNid && listeCaseNid.Count() > 0) // si cherche le nid et qu'il est a proximité, on décharge la mule
             {
                 porteSucre = false;
                 chercheNid = false;
@@ -113,22 +113,22 @@ namespace Fourmiliere
             if (cptCasesValides == 0) //Aucune case n'est valide on sort de la fonction
                 return;
 
-            if (chercheSucre && listePheroSucre.Count()>0) //La fourmi suit la piste du sucre
+            if (chercheSucre && listePheroSucre.Count() > 0) //La fourmi suit la piste du sucre
             {
                 int pheroMax = 0;
                 int index = -1;
                 int cpt = 0;
                 foreach (Case ca in listePheroSucre)
                 {
-                    
-                    if(ca.pheromone_sucre>pheroMax && CaisseAOut.CaseValidePourFourmis(ca))
+
+                    if (ca.pheromone_sucre > pheroMax && CaisseAOut.CaseValidePourFourmis(ca))
                     {
-                        pheroMax = ca.pheromone_sucre; 
+                        pheroMax = ca.pheromone_sucre;
                         index = cpt;
                     }
                     cpt++;
                 }
-                if (index > -1 && listePheroSucre[index].pheromone_sucre>=caseFourmi.pheromone_sucre)
+                if (index > -1 && listePheroSucre[index].pheromone_sucre >= caseFourmi.pheromone_sucre)
                 {
                     DeplacerFourmis(listePheroSucre[index].X, listePheroSucre[index].Y);
                     return;
@@ -138,13 +138,16 @@ namespace Fourmiliere
                     DeplacementAleatoire();
                     return;
                 }
-                  
+
             }
-            else if(chercheNid && listePheroNid.Count()>0) //La fourmi suit la piste du Nid
+            else if (chercheNid && listePheroNid.Count() > 0) //La fourmi suit la piste du Nid
             {
-                int pheroMax = 0; //a faire : choisir la case la plus proche du nid si égalité de pheromone
+                int pheroMax = 0;
                 int index = -1;
                 int cpt = 0;
+
+                List<Case> listePheroNidMax = new List<Case>();
+
                 foreach (Case ca in listePheroNid)
                 {
                     if (ca.pheromone_nid > pheroMax && CaisseAOut.CaseValidePourFourmis(ca))
@@ -153,12 +156,20 @@ namespace Fourmiliere
                         index = cpt;
                     }
                     cpt++;
+                }
+                foreach (Case ca in listePheroNid)
+                {
+                    if (ca.pheromone_nid == pheroMax)
+                    {
+                        listePheroNidMax.Add(ca);
+                    }
 
                 }
+                Case caseFinale = ChoixCaseProcheNid(listePheroNidMax);
                 if (index > -1)
                 {
                     DepotDePheromoneSucre();
-                    DeplacerFourmis(listePheroNid[index].X, listePheroNid[index].Y);
+                    DeplacerFourmis(caseFinale.X, caseFinale.Y);
                     return;
                 }
                 else
@@ -176,7 +187,7 @@ namespace Fourmiliere
 
 
         public void DeplacementAleatoire()
-        {            
+        {
             int x;
             int y;
             do
@@ -184,7 +195,7 @@ namespace Fourmiliere
                 do
                 {
                     x = caseFourmi.X;
-                    
+
                     int rndX = rnd.Next(3);
                     if (rndX == 0)
                     {
@@ -225,10 +236,14 @@ namespace Fourmiliere
 
         private void DeplacerFourmis(int x, int y)
         {
-            
-            RefTableau.tab[caseFourmi.X, caseFourmi.Y].fourmis = null;
-            RefTableau.tab[x, y].fourmis = this;
-            caseFourmi = RefTableau.tab[x, y];
+            if (CaisseAOut.CaseValidePourFourmis(RefTableau.tab[x, y]))
+            {
+                RefTableau.tab[x, y].fourmis = this;
+
+                RefTableau.tab[caseFourmi.X, caseFourmi.Y].fourmis = null;
+                caseFourmi = RefTableau.tab[x, y];
+            }
+
         }
 
 
@@ -239,7 +254,41 @@ namespace Fourmiliere
             pheroSucreVal--;
         }
 
-      
+
+        private Case ChoixCaseProcheNid(List<Case> casesPotentielles)
+        {
+            Case casePlusProche = casesPotentielles[0];
+            int distanceMin = 999;
+            int nidX = Tableau.posNidStatic[0];
+            int nidY = Tableau.posNidStatic[1];
+            foreach (Case c in casesPotentielles)
+            {
+                int distX = 0;
+                int distY = 0;
+
+
+                if (c.X > nidX)
+                    distX = c.X - nidX;
+                else
+                    distX = nidX - c.X;
+
+
+                if (c.Y > nidY)
+                    distY = c.Y - nidY;
+                else
+                    distY = nidY - c.Y;
+
+
+                if (distX + distY < distanceMin)
+                {
+                    casePlusProche = c;
+                    distanceMin = distX + distY;
+                }
+
+            }
+            return casePlusProche;
+        }
+
 
     }
 }
